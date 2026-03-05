3ae7795df80f43076d5891183f30d86735df2d73
Ruslam123 <ruslanyacenko2003@gmail.com>
2026-03-05 11:04:25 +0200

    20

---
+++ FlowSign.Infrastructure/Persistence/FlowSignDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace FlowSign.Infrastructure.Persistence;
+using FlowSign.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class FlowSignDbContext : DbContext
+{
+    public DbSet <User> Users { get; set; }
+    public DbSet <Document> Documents { get; set; }
+    public DbSet <DocumentVersion> DocumentVersions { get; set; }
+    public DbSet <WorkflowInstance> WorkflowInstances { get; set; }
+    public DbSet <SignatureRequest> SignatureRequests { get; set; }
+    public DbSet <AuditLog> AuditLogs { get; set; }
+
+    public FlowSignDbContext(DbContextOptions<FlowSignDbContext> options) : base(options)
+    {
+    }
+
+
+}
