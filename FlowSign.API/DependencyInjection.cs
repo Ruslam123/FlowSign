fee71eef134bc1faa3b773b9a315e9570cc917f3
Ruslam123 <ruslanyacenko2003@gmail.com>
2026-03-08 20:48:44 +0200

    36

--- FlowSign.API/DependencyInjection.cs
+++ FlowSign.API/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿namespace FlowSign.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using FlowSign.Application.Interfaces.Repositories;
