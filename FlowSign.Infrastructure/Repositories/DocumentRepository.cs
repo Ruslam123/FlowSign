cd7208d54c6b0714874b55620c9526361acb501d
Ruslam123 <ruslanyacenko2003@gmail.com>
2026-03-08 15:27:30 +0200

    27

--- FlowSign.Infrastructure/Repositories/DocumentRepository.cs
+++ FlowSign.Infrastructure/Repositories/DocumentRepository.cs
@@ -1,35 +1,39 @@
 ﻿namespace FlowSign.Infrastructure.Repositories;
 using FlowSign.Application.Interfaces.Repositories;
-using FlowSign.Infrastructure.Persistence.Configurations;
-
+using Microsoft.EntityFrameworkCore;
+using FlowSign.Domain.Entities;
+using FlowSign.Infrastructure.Persistence;
 
 public class DocumentRepository : IDocumentRepository
 {
-	public readonly ApplicationDbContext _context;
-    public async Task<Document?> GetByIdAsync(Guid id)
-		{
-			return await _context.Documents.Include(d => d.Version).Include(d => d.SignatureRequests).FirstOrDefaultAsync(d => d.Id == id);
-		}
-		public async Task<List<Document>> GetByOwnerAsync(Guid ownerId, int page, int pageSize)
-		{
-			return await _context.Documents.Where(d => d.OwnerId == ownerId)
-				.Skip((page - 1) * pageSize)
-				.Take(pageSize)
-				.ToListAsync();
-		}
-		public async Task AddAsync(Document document)
-		{
-			await _context.Documents.AddAsync(document);
-			await _context.SaveChangesAsync();
-		}
-		public async Task UpdateAsync(Document document)
-		{
-			_context.Documents.Update(document);
-			await _context.SaveChangesAsync();
-		}
-		public async Task<bool> IsOwnerAsync(Guid userId, Guid documentId)
-		{
-			var document = await _context.Documents.FindAsync(documentId);
-			return document != null && document.OwnerId == userId;
-    }
+	public readonly FlowSignDbContext _context;
+	public DocumentRepository(FlowSignDbContext context)
+	{
+		_context = context;
+	}
+	public async Task<Document?> GetByIdAsync(Guid id)
+	{
+		return await _context.Documents.Include(d => d.Versions).Include(d => d.SignatureRequests).FirstOrDefaultAsync(d => d.Id == id);
+	}
+	public async Task<List<Document>> GetByOwnerAsync(Guid ownerId, int page, int pageSize)
+	{
+		return await _context.Documents.Where(d => d.OwnerId == ownerId)
+			.Skip((page - 1) * pageSize)
+			.Take(pageSize)
+			.ToListAsync();
+	}
+	public async Task AddAsync(Document document)
+	{
+		await _context.Documents.AddAsync(document);
+		await _context.SaveChangesAsync();
+	}
+	public async Task UpdateAsync(Document document)
+	{
+		_context.Documents.Update(document);
+		await _context.SaveChangesAsync();
+	}
+	public async Task<bool> IsOwnerAsync(Guid userId, Guid documentId)
+	{
+		return await _context.Documents.AnyAsync(d => d.Id == documentId && d.OwnerId == userId);
+	}
 }
